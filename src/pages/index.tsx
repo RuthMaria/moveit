d972a56285385335782ff9a5df93c9ead9928504
RuthMaria <ruth-15rms@hotmail.com>
2021-02-23 14:21:44 -0300

    created completedChallenges component

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import { ExperienceBar } from "../components/ExperienceBar";
+import Head from 'next/head'
+import { Profile } from '../components/Profile'
+import styles from '../styles/pages/Home.module.css'
+import { CompletedChallenges } from "../components/CompletedChallenges";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+        </div>
+        <div></div>
+      </section>
+    </div>
+  )
+}
